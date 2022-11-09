a5b3c43e9394c690c35e8e14032c33e9a60b1c1b
ArtemMakovskyy <photomak79@gmail.com>
2022-11-09 10:34:19 +0200

    09-11-2022

--- src/CalaculatedText.java
+++ src/CalaculatedText.java
@@ -2,7 +2,7 @@ public class CalaculatedText {
     public static void main(String[] args) {
         PlusMinus p = new PlusMinus();
         p.rezult("(1+2)*3-(4*(5+6*7)/(8-7))");
-        p.rezult(".99+.66546");
+        p.rezult("0.99+0.66546");
         p.rezult("-25.99+1.66546");
     }
 }
